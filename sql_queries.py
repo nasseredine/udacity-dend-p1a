2dd9412b8e684ad5595d3784f2bad28d32d0b25b
Nasseredine Bajwa <fr.bajwa.nasseredine@gmail.com>
2020-05-28 13:12:33 +0200

    feat: write CREATE statements in sql_queries to create each table

--- sql_queries.py
+++ sql_queries.py
@@ -9,18 +9,62 @@ time_table_drop = ""
 # CREATE TABLES
 
 songplay_table_create = ("""
+CREATE TYPE membership_level AS ENUM ('free', 'paid');
+
+CREATE TABLE IF NOT EXISTS songplays (
+    songplay_id int PRIMARY KEY,
+    start_time timestamp NOT NULL,
+    level membership_level NOT NULL,
+    song_id varchar NOT NULL,
+    artist_id varchar NOT NULL,
+    session_id int NOT NULL,
+    location varchar NOT NULL,
+    user_agent varchar NOT NULL
+);
 """)
 
 user_table_create = ("""
+CREATE TYPE users_gender AS ENUM ('F', 'M');
+
+CREATE TABLE IF NOT EXISTS users (
+    user_id int PRIMARY KEY,
+    first_name varchar NOT NULL,
+    last_name varchar NOT NULL,
+    gender users_gender NOT NULL,
+    level membership_level NOT NULL
+);
 """)
 
 song_table_create = ("""
+CREATE TABLE IF NOT EXISTS songs (
+    song_id char(18) PRIMARY KEY,
+    title varchar NOT NULL,
+    artist_id char(8) NOT NULL,
+    year int,
+    duration numeric(8,5)
+);
 """)
 
 artist_table_create = ("""
+CREATE TABLE IF NOT EXISTS artists (
+    artist_id char(18) PRIMARY KEY,
+    name varchar NOT NULL,
+    location varchar,
+    latitude numeric(7,5),
+    longitude numeric(7,5)
+);
 """)
 
 time_table_create = ("""
+CREATE TABLE IF NOT EXISTS time (
+    start_time timestamp PRIMARY KEY,
+    hour int NOT NULL,
+    day int NOT NULL,
+    week int NOT NULL,
+    month int NOT NULL,
+    year int NOT NULL,
+    weekday int NOT NULL
+);
 """)
 
 # INSERT RECORDS

da6e5886cbd0976b82562f1991bd3940ae8d7c96
Nasseredine Bajwa <fr.bajwa.nasseredine@gmail.com>
2020-05-28 21:05:46 +0200

    feat(etl): process song_data for songs and artists tables

--- sql_queries.py
+++ sql_queries.py
@@ -81,9 +81,13 @@ user_table_insert = ("""
 """)
 
 song_table_insert = ("""
+INSERT INTO songs (song_id, title, artist_id, year, duration)
+VALUES (%s, %s, %s, %s, %s);
 """)
 
 artist_table_insert = ("""
+INSERT INTO artists (artist_id, name, location, latitude, longitude)
+VALUES (%s, %s, %s, %s, %s);
 """)
 
 

69646282ce2b5c4c0b5c253bc8a374843ad647a7
Nasseredine Bajwa <fr.bajwa.nasseredine@gmail.com>
2020-05-28 13:27:57 +0200

    feat: write DROP statements in sql_queries.py to drop each table

--- sql_queries.py
+++ sql_queries.py
@@ -1,10 +1,15 @@
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = """
+DROP TYPE IF EXISTS membership_level;
+DROP TABLE IF EXISTS songplays;
+"""
+user_table_drop = """
+DROP TYPE IF EXISTS users_gender;
+DROP TABLE IF EXISTS users;"""
+song_table_drop = "DROP TABLE IF EXISTS songs;"
+artist_table_drop = "DROP TABLE IF EXISTS artists;"
+time_table_drop = "DROP TABLE IF EXISTS time;"
 
 # CREATE TABLES
 
